66529d5d188e7ecea006e46c9e242e05fdd1112b
SimoneDeCillis <116493653+SimoneDeCillis@users.noreply.github.com>
2022-12-08 22:09:53 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,21 +1,14 @@
 #include "lib.h"
 
-
 bool conversione(char &c) {
-
     if ((c >= 0) && (c <= 9)) {
         return false;
-
-
     }
     else
     {
-
-
         if(c>= 'A' && c <= 'Z'){
             c= c + 32;
             return true;
-
         }
         else if((c>='a')&&(c<='z')){
             c=c-32;
