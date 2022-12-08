ef8514fb7479bafd158c67602a8ce527d893118c
SimoneDeCillis <116493653+SimoneDeCillis@users.noreply.github.com>
2022-12-08 22:02:15 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
-
+using namespace std;
+#include "lib.h"
 int main(){
+   char a;
+    bool t;
+cin>>a;
+
+t= conversione(a);
+if(t== true){
+    cout<<a<<endl;
+}
+else{
+    cout<<"errore"<<endl;
+}
   return 0;
 }
